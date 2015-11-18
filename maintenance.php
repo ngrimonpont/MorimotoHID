66f53bdb2fcda50092425a721f9903f550eafad5
Rob Shimits | reshimits <r.e@shimits.com>
2015-11-18 12:53:29 -0500

    Site Maintenance page

---
+++ maintenance.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+
+<head>
+
+	<!-- Basic Page Needs
+    ================================================== -->
+	<meta charset="utf-8">
+	<title>Down for Maintenance • MORIMOTO • High Quality HID Projectors, Ballasts, and Bulbs</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="" />
+	<meta name="author" content="SHIMITS Design/Development">
+
+	<!-- Mobile Specific Metas
+    ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta name="format-detection" content="telephone=no" />
+
+	<!-- CSS
+    ================================================== -->
+    <link href="css/bootstrap-theme-full.css" rel="stylesheet">
+    <link href="css/morimoto-theme.css" rel="stylesheet">
+
+	<!-- Javascript [primary]
+    ================================================== -->
+  	<script src="js/modernizr.custom.63321.js"></script>
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+
+	<!-- Favicons
+    ================================================== -->
+	<link rel="shortcut icon" href="img/favicon.ico">
+    <link rel="icon" sizes="32x32" href="img/favicon-32.png">
+	<link rel="apple-touch-icon-precomposed" sizes="152x152" href="img/favicon-152.png">
+
+</head>
+
+<body class="utility">
+
+    <!-- Header
+    ================================================== -->
+
+
+
+    <!-- Primary Page Layout
+    ==================================================
+    ==================================================
+    ================================================== -->
+
+        
+        <div class="section description intro">
+            <div class="container">
+                <div class="row">
+                    <div class="lead col-xs-12">
+                        <h5>Oh No!</h5>
+                        <h2>Site Maintenance</h2>
+                        <p><span class="lead">We sincerely apologize for the inconvenience. </br>
+                        Our site is currently undergoing scheduled maintenance and updates, but will return shortly. Thank you for you patience.</span></br>
+                        <span class="small">If you have any immediate questions, please contact <a href="mailto:info@morimotohid.com?subject=Morimoto Maintenance Page Email:" class="email">info@morimotohid.com</a>.</span></p>
+
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+
+
+ 
+
+  
+    
+        
+
+
+    <!-- End Primary Page Layout
+    ==================================================
+    ==================================================
+    ================================================== -->
+
+    
+
+	<!-- Javascript [secondary]
+  	================================================== -->
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>    
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+
+    <!-- ALL: iOS Viewport Fix -->
+    <script type="text/javascript">
+    var iOS = navigator.userAgent.match(/(iPod|iPhone|iPad)/);
+    if(iOS){
+
+       function iosVhHeightBug() {
+          var height = $(window).height();
+          $(".dropdown-menu").css('min-height', height);
+       }
+
+       iosVhHeightBug();
+       $(window).bind('resize', iosVhHeightBug);
+    }  
+    </script> 
+    
+    
+
+
+    <!-- End Document
+    ================================================== -->
+
+</body>
+</html>
